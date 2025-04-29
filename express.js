d76739195e5d9b1a0ccd37eda633ce955028d372
Gropp <fgropp@gmail.com>
2025-04-29 16:42:24 -0300

    express

---
+++ express.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+// porta onde o node vai ser liberado
+app.listen(3000)
